0fb88d462112a6c8dde44fd94f97719490d8ead3
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-12-08 22:48:59 +0300

    Update water_saver_arduino.cpp

--- water_saver_arduino.cpp
+++ water_saver_arduino.cpp
@@ -1,2 +1,6 @@
 #include<Wire.h>
 // Defining the constants required
+const int motionSensorPin = 2; // pin to which the motion sensor is connected
+const flushRelayPin = 3; // pin to which relay is connected
+
+unsigned long lastMotionTime = 0;
